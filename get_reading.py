56a2a0914dede17d5bd71b6f2318ace1b81c2b7d
benhoyle <benjhoyle@gmail.com>
2016-01-27 11:00:55 +0000

    Add error handling

--- get_reading.py
+++ get_reading.py
@@ -15,33 +15,39 @@ def main():
 	timeout 	=	10
 	
 	while True:
-		meter		=	serial.Serial(port, baud, timeout=timeout)
-		data		=	meter.readline()
-		meter.close()
-		
-		#print data # For testing
-		
-		watts_ex	= 	re.compile('<watts>([0-9]+)</watts>')
-		temp_ex		= 	re.compile('<tmpr>([0-9\.]+)</tmpr>')
-		
-		raw_watts	=	watts_ex.findall(data)
-		if raw_watts:
-			watts 	= 	int(raw_watts.pop())
-			#print watts # For testing
+		try:
+			meter		=	serial.Serial(port, baud, timeout=timeout)
+			data		=	meter.readline()
+			meter.close()
+		except SerialException:
+			print "Could not access device. Trying again in 30s. \n"
+			# Try again  in 30s
+			time.sleep(30)
 			
-		raw_temp	=	temp_ex.findall(data)
-		if raw_temp:
-			temp	=	float(raw_temp.pop())
-			#print temp # For testing
-		
-		if watts and temp:
-			reading 	=	Reading(watts, temp)
-			#print "Adding Reading" # For testing
-			db.session.add(reading)
-			db.session.commit()
-		
-		# Sleep for 2 minutes
-		time.sleep(120)
+		else:
+			#print data # For testing
+			
+			watts_ex	= 	re.compile('<watts>([0-9]+)</watts>')
+			temp_ex		= 	re.compile('<tmpr>([0-9\.]+)</tmpr>')
+			
+			raw_watts	=	watts_ex.findall(data)
+			if raw_watts:
+				watts 	= 	int(raw_watts.pop())
+				#print watts # For testing
+				
+			raw_temp	=	temp_ex.findall(data)
+			if raw_temp:
+				temp	=	float(raw_temp.pop())
+				#print temp # For testing
+			
+			if watts and temp:
+				reading 	=	Reading(watts, temp)
+				#print "Adding Reading" # For testing
+				db.session.add(reading)
+				db.session.commit()
+			
+			# Sleep for 2 minutes
+			time.sleep(120)
 	
 if __name__ == "__main__":
 	main()
